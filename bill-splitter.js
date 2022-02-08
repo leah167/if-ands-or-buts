e36bc17aaf3b7d17654915106d9864d87a3ebd79
Leah Sanchez <leahsanchez@Leahs-MacBook-Pro.local>
2022-02-08 14:39:10 -0700

    fixed bill-splitter

--- bill-splitter.js
+++ bill-splitter.js
@@ -7,9 +7,9 @@ let Person2 = Number(prompt('Amount Person 2 paid: '));
 if (Person1 - Person2 === 0) {
     console.log("Bill is split evenly")
 } else if ((Person1 - Person2) >= 0) {
-    let result = (total - Person1)
+    let result = ((total/2) - Person2)
     console.log("Person 2 owes Person 1 " + result + " dollars. ")
 } else if ((Person1 - Person2) <= 0) {
-    let result = (total - Person2)
+    let result = ((total/2) - Person1)
     console.log("Person 1 owes Person 2 " + result + " dollars. ")
 }
