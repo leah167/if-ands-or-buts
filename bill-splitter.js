621f7c5b1ab03431118ff85d74fe34e59a4c6e86
Leah Sanchez <leahsanchez@Leahs-MacBook-Pro.local>
2022-02-08 13:51:59 -0700

    add completed bill-splitter

---
+++ bill-splitter.js
@@ -0,0 +1,15 @@
+const prompt = require('prompt-sync')();
+
+let total = Number(prompt('Enter bill total: '));
+let Person1 = Number(prompt('Amount Person 1 paid: '));
+let Person2 = Number(prompt('Amount Person 2 paid: '));
+
+if (Person1 - Person2 === 0) {
+    console.log("Bill is split evenly")
+} else if ((Person1 - Person2) >= 0) {
+    let result = (total - Person1)
+    console.log("Person 2 owes Person 1 " + result + " dollars. ")
+} else if ((Person1 - Person2) <= 0) {
+    let result = (total - Person2)
+    console.log("Person 1 owes Person 2 " + result + " dollars. ")
+}
